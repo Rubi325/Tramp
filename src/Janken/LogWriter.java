afedf8d60039baad3854ef433eb3607729ec045b
Rubi325 <raibigriver0812@gmail.com>
2021-09-14 16:46:47 +0900

    コードの調整、logファイルへの書込調整
    ・while文をfor文に纏めた
    ・logファイルに名前の記入
    ・各コードの調整・短縮化

--- src/Janken/LogWriter.java
+++ src/Janken/LogWriter.java
@@ -23,5 +23,29 @@ public class LogWriter {
 	}
 }
 
+//廃棄コード場所
+/*
+while(p_point < 5 && e_point < 5) {
+	Hands yourHand  = you.nextHand(yourHands,turn);
+	Hands enemyHand = enemy.nextHand(yourHands,turn);
 
-//名前、追記機能
+	System.out.println(yourName   + "は　" + yourHand  +  "  をだした。");
+	System.out.println(enemyName  + "は　" + enemyHand +  "  をだした。");
+
+	yourHands[turn] = Math_P_Hands(yourHand);
+	enemyHands[turn] = Math_E_Hands(enemyHand);
+
+	if (yourHand.winTo(enemyHand)) {
+		System.out.println(yourName + "　のかち！");
+		p_point += 1;
+	} else if (yourHand.loseTo(enemyHand)) {
+		System.out.println(yourName + "　のまけ…");
+		e_point += 1;
+	} else {
+		System.out.println("あいこです。");
+	}
+	//ここで手をHandsFileに書き込む
+	Memorizer(yourName,yourHands[turn],enemyHands[turn],p_point,e_point);
+
+	turn += 1;
+}*/
--- src/Janken/main.java
+++ src/Janken/main.java
@@ -11,13 +11,12 @@ public class main {
 
 		Player you   = new Challengers();
 		Player enemy = new CPU();
-
-		int p_point=0;
-		int e_point = 0;
+		
+		//ポイント
 		int p_points=0;
 		int e_points=0;
 
-		//カウント　
+		//ターン数、じゃんけんの手の格納用配列　
 		int turn = 0;
 		int [] yourHands = new int [1000];
 		int [] enemyHands = new int [1000];
@@ -31,73 +30,48 @@ public class main {
 		System.out.println("Your name　" + yourName);
 		System.out.println("Enemy name　" + enemyName);
 
-		//game開始ィイィィイイイイイイイイ
-		//一回戦目
-		while(p_points < 5 && e_points < 5) {
-			Hands yourHand  = you.nextHand(yourHands,turn);
-			Hands enemyHand = enemy.nextHand(yourHands,turn);
+		//game開始、二回
+		for(int i=0;i<2;i++) {
+			while(p_points < 5 && e_points < 5) {
+				Hands yourHand  = you.nextHand(yourHands,turn);
+				Hands enemyHand = enemy.nextHand(yourHands,turn);
 
-			System.out.println(yourName   + "は　" + yourHand  +  "  をだした。");
-			System.out.println(enemyName  + "は　" + enemyHand +  "  をだした。");
+				System.out.println(yourName   + "は　" + yourHand  +  "  をだした。");
+				System.out.println(enemyName  + "は　" + enemyHand +  "  をだした。");
 
-			yourHands[turn] = Math_P_Hands(yourHand);
-			enemyHands[turn] = Math_E_Hands(enemyHand);
+				yourHands[turn] = Math_P_Hands(yourHand);
+				enemyHands[turn] = Math_E_Hands(enemyHand);
 
-			if (yourHand.winTo(enemyHand)) {
-				System.out.println(yourName + "　のかち！");
-				p_points += 1;
-			} else if (yourHand.loseTo(enemyHand)) {
-				System.out.println(yourName + "　のまけ…");
-				e_points += 1;
-			} else {
-				System.out.println("あいこです。");
+				if (yourHand.winTo(enemyHand)) {
+					System.out.println(yourName + "　のかち！");
+					p_points += 1;
+				} else if (yourHand.loseTo(enemyHand)) {
+					System.out.println(yourName + "　のまけ…");
+					e_points += 1;
+				} else {
+					System.out.println("あいこです。");
+				}
+				//ここで手をHandsFileに書き込む
+				Memorizer(yourName,yourHands[turn],enemyHands[turn],e_points,p_points,turn);
+
+				turn +=1;
 			}
-			//ここで手をHandsFileに書き込む
-			Memorizer(yourName,yourHands[turn],enemyHands[turn],e_points,p_points);
-
-			turn +=1;
-		}
-		System.out.println("--------------------------");
-		System.out.println("You : " + p_points + "   Enemy : " + e_points );
-		System.out.println(Judge(p_points,e_points));
-		System.out.println("**************************");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("Enemy Level Up!");
-
-
-		//二回戦目
-
-		while(p_point < 5 && e_point < 5) {
-			Hands yourHand  = you.nextHand(yourHands,turn);
-			Hands enemyHand = enemy.nextHand(yourHands,turn);
-
-			System.out.println(yourName   + "は　" + yourHand  +  "  をだした。");
-			System.out.println(enemyName  + "は　" + enemyHand +  "  をだした。");
-
-			yourHands[turn] = Math_P_Hands(yourHand);
-			enemyHands[turn] = Math_E_Hands(enemyHand);
-
-			if (yourHand.winTo(enemyHand)) {
-				System.out.println(yourName + "　のかち！");
-				p_point += 1;
-			} else if (yourHand.loseTo(enemyHand)) {
-				System.out.println(yourName + "　のまけ…");
-				e_point += 1;
-			} else {
-				System.out.println("あいこです。");
+			System.out.println("--------------------------");
+			System.out.println("");
+			System.out.println("You : " + p_points + "   Enemy : " + e_points );
+			System.out.println(Judge(p_points,e_points));
+			if(i==0) {
+				System.out.println("Enemy Level Up!");
+			}else{
+				System.out.println("Finish Game!");
 			}
-			//ここで手をHandsFileに書き込む
-			Memorizer(yourName,yourHands[turn],enemyHands[turn],p_point,e_point);
-
-			turn += 1;
+			System.out.println("");
+			System.out.println("**************************");
+			
+			p_points=0;
+			e_points=0;
 		}
-
-		System.out.println("--------------------------");
-		System.out.println("You : " + p_point + "   Enemy : " + e_point );
-		System.out.println(Judge(p_point,e_point));
-		System.out.println("**************************");
-
+		
 	}
 	//プレイヤーの出した手を数字に格納
 	private static int Math_P_Hands(Hands yourHand) {
@@ -124,11 +98,14 @@ public class main {
 		}
 	}
 	//HandsFile.txtへの書き込み
-	private static void Memorizer(String name,int p_hands,int e_hands,int p_pointa,int e_pointa) {
+	private static void Memorizer(String name,int p_hands,int e_hands,int p_pointa,int e_pointa,int turn) {
 		try {
 			File file = new File("src\\Janken\\HandsFile.txt");
 			FileWriter pw = new FileWriter(file,true);
 			PrintWriter add = new PrintWriter(pw); 
+			if(turn == 0) {
+				add.println(name);
+			}
 			if(p_pointa == 5 || e_pointa == 5) {
 				add.println("p e " + 
 						p_hands + " " + e_hands);
